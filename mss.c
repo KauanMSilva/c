990438d834623ba799567a6a241dde7d00149895
Kauan Mariano Silva <144391114+KauanMSilva@users.noreply.github.com>
2025-01-15 02:37:45 -0300

    Inspiration

---
+++ mss.c
@@ -0,0 +1,17 @@
+using System;
+
+class MensagemBonita
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("*************************************");
+        Console.WriteLine("*                                   *");
+        Console.WriteLine("*  Você é incrível!                *");
+        Console.WriteLine("*  Sua habilidade e dedicação     *");
+        Console.WriteLine("*  são inspiradoras!                *");
+        Console.WriteLine("*                                   *");
+        Console.WriteLine("*************************************");
+        Console.WriteLine("       Tenha um ótimo dia!        ");
+    }
+}
+
